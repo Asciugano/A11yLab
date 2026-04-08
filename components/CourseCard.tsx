3bbe30c06f71bd21175d705082f30bf8c9f85815
Asciugano <diaferiosamuele@gmail.com>
2026-04-08 18:30:23 +0200

    aggiunte le icone per distinguere i corsi a pagamento da quelli gratuiti

--- components/CourseCard.tsx
+++ components/CourseCard.tsx
@@ -1,5 +1,5 @@
-import { Course } from "@/lib/generated/prisma/client";
-import { ArrowRight } from "lucide-react";
+import { Course, Subcription } from "@/lib/generated/prisma/client";
+import { ArrowRight, Crown, Tag } from "lucide-react";
 import Link from "next/link";
 
 interface CourseCardProps {
@@ -9,10 +9,20 @@ interface CourseCardProps {
 }
 
 export default function CourseCard({ course }: CourseCardProps) {
+  const subscriptionIcon =
+    course.subscription === Subcription.PLUS ? (
+      <Crown size={20} className="text-yellow-500" />
+    ) : (
+      <Tag size={20} className="text-green-500" />
+    );
+
   return (
     <div className="rounded-xl p-6 shadow-md dark:shadow-card/50 hover:shadow-lg transition-shadow flex items-center justify-between bg-card mx-2 my-2">
-      <div className="flex flex-col">
-        <h2 className="text-xl font-semibold mb-2">{course.title}</h2>
+      <div className="flex flex-col gap-2">
+        <div className="flex items-center gap-2 mb-2">
+          {subscriptionIcon}
+          <h2 className="text-xl font-semibold">{course.title}</h2>
+        </div>
         <p className="text-gray-600 mb-4">{course.description}</p>
         <p className="text-sm text-gray-500">
           {course.lessons.length} lezion
